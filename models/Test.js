24597b715dff0a6d0636ecc6581982d66e31d194
dylanhale <dylanhale@outlook.com>
2022-04-20 22:13:44 -0500

    Delete Test.js model
    Rehash is completed, merged into main branch. Unused Test.js model removed

--- models/Test.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const TestSchema = mongoose.Schema({
-    _id: String,
-    name: String,
-    age: Number
-})
-
-module.exports = mongoose.model('TestScheme',TestSchema)
